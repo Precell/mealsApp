769a2bc49050d660b7c0c045e7f80f58a1327fb1
Precell <precell@uncommon.org>
2024-10-15 08:43:12 -0400

    Refactor MealItem component to display additional meal details

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,13 +1,18 @@
 import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 import React from "react";
 
-const MealItem = ({ title, imageUrl }) => {
+const MealItem = ({ title, imageUrl, duration, complexity, affordability }) => {
   return (
     <View>
       <Pressable>
-        <View> 
-            <Image source={{uri:imageUrl}} style={styles.image}/>
-          <Text>{title}</Text>
+        <View>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <View>
+            <Text>{duration}n</Text>
+            <Text>{complexity.toUpperCase()}</Text>
+            <Text>{affordability.toUpperCase()}</Text>
         </View>
       </Pressable>
     </View>
@@ -17,13 +22,13 @@ const MealItem = ({ title, imageUrl }) => {
 export default MealItem;
 
 const styles = StyleSheet.create({
-    image:{
-        width:'100%',
-        height: 200
-    },
-    title:{
-        fontWeight:'bold',
-        textAlign:'center',
-        fontSize:18
-    }
-})
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+  },
+});
