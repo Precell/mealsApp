4ead737fddbe41c6ba0b5d22a94e3985c4c10c1f
Precell <precell@uncommon.org>
2024-10-15 08:31:13 -0400

    Refactor MealsOverVIewScreen to display meal item images

--- screens/MealsOverVIewScreen.js
+++ screens/MealsOverVIewScreen.js
@@ -18,7 +18,7 @@ export default function MealsOverVIewScreen({ route }) {
 
 
     function renderMealItem(itemData) {
-        return <MealItem title={itemData.item.title}/>
+        return <MealItem title={itemData.item.title} imageUrl={itemData.item.imageUrl}/>
     }
   return (
     <View style={styles.container}>
